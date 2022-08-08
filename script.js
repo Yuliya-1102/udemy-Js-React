cb85a1e2a8cbb8e1d6137afe6e992a599c59bf77
Babitskaya <bambina-kissi@yandex.ru>
2022-08-08 12:49:50 +0300

    lesson19

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 "use strict";
 
-const hell = 2;
-console.log(hell);
+const numberofFilms = +prompt('How much movies did you watch?','');
+
+const personalMovieDB = {
+    'count': numberofFilms,
+    'movies': {},
+    'actors': {},
+    'genres': [],
+    'privat': false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?','');
+const b = +prompt('На сколько оцените его?','');
+
+personalMovieDB.movies[a] = b;
+console.log(personalMovieDB);
